0bf9caffbf2356fe7325447c3a079583ec580e0d
RISHI RAWAT <sl177y.log0@gmail.com>
2025-01-24 19:22:08 +0530

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-          <p className=' text-gray-500'>SHAITAN GALI, KHATRA MAHEL <br /> GALI NUMBER 420, ANDHER NAGAR, SHAMSHAN KE SAAMNE</p>
+          <p className=' text-gray-500'>RISHI RAWAT <br /> SHAITAN GALI, KHATRA MAHEL <br /> GALI NUMBER 420, ANDHER NAGAR, SHAMSHAN KE SAAMNE</p>
           <p className=' text-gray-500'>Tel: +919280085420 <br /> Email: sl177y.log0@gmail.com</p>
           <p className='font-semibold text-xl text-gray-600'>Careers at KAPDA</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings which is not available at all.</p>
