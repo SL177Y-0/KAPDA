3a18268eba58302c5035b0a2a85f70f0ba0e0d96
RISHI RAWAT <sl177y.log0@gmail.com>
2025-02-03 17:46:27 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,20 +4,14 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import ShopContextProvider from './context/ShopContext.jsx';
-import { ClerkProvider } from '@clerk/clerk-react';
-
-// Load the Clerk publishable key from environment variables
-const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 // Render the app
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={clerkPubKey}>
-      <BrowserRouter>
-        <ShopContextProvider>
-          <App />
-        </ShopContextProvider>
-      </BrowserRouter>
-    </ClerkProvider>
+    <BrowserRouter>
+      <ShopContextProvider>
+        <App />
+      </ShopContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
